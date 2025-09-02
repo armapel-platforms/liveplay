ee58fd061bc0bd3c071feba6f351a4d70de7b3ca
Arbie Peligro <arbiemapalopeligro@gmail.com>
2025-09-03 00:00:12 +0800

    Update yt-live.js

--- js/yt-live.js
+++ js/yt-live.js
@@ -1,33 +1,7 @@
 const yt_live = [
-  { 
-    name: 'Kapamilya Online Live', 
-    type: 'youtube', 
-    embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCstEtN0pgOmCf02EdXsGChw', // <--- HERE
-    logo: 'https://static.wikia.nocookie.net/logopedia/images/0/00/Kapamilya_Online_Live_3D_Logo_2020.png', 
-    category: "YOUTUBE LIVE" 
-  },
-  { 
-    name: 'Kapuso Stream', 
-    type: "youtube", 
-    embedUrl: "https://www.youtube.com/embed/live_stream?channel=UCKL5hAuzgFQsyrsQKgU0Qng", // <--- HERE
-    logo: 'https://static.wikia.nocookie.net/gmanetwork/images/0/0f/Kapuso_Stream.png', 
-    category: "YOUTUBE LIVE" 
-  },
-  { 
-    name: 'DZRH TV', 
-    type: "youtube", 
-    embedUrl: 'https://www.youtube.com/embed/8GZM3fhhC18?si=yyuxNxEsLkSoXg4B&amp', // <--- HERE
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/DZRH_News_Television_Logo_%282021%29.jpg?20220522024804', 
-    category: "YOUTUBE LIVE" 
-  },
-  {
-    name: 'THE K-POP',
-    type: "youtube",
-    embedUrl: 'https://www.youtube.com/embed/JVocS7Yftw8?si=hSZxg1vZZB-xm9MN',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyIEniR-KegRyA_uAbvY7T0w7e0zMQTiX4ew&usqp=CAU',
-    category: "YOUTUBE LIVE"
-  },
-  {
-    name: 'THE K-POP 2', type: "youtube", embedUrl: 'https://www.youtube.com/embed/8fHegYsHwZQ?si=ZLSTB-VofWf2gbXR', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVY2Fz89o35Wx6ImHq8HVKrfp8dF-YXxrTGD6LcG7UZRclgSvkfnMXuYQ&s=10', category: "YOUTUBE LIVE"
-  }
+  { name: 'Kapamilya Online Live', type: 'youtube', embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCstEtN0pgOmCf02EdXsGChw', logo: 'https://static.wikia.nocookie.net/logopedia/images/0/00/Kapamilya_Online_Live_3D_Logo_2020.png', category: "YouTube Live" },
+  { name: 'Kapuso Stream', type: "youtube", embedUrl: "https://www.youtube.com/embed/live_stream?channel=UCKL5hAuzgFQsyrsQKgU0Qng", logo: 'https://static.wikia.nocookie.net/gmanetwork/images/0/0f/Kapuso_Stream.png', category: "YouTube Live" },
+  { name: 'DZRH TV', type: "youtube", embedUrl: 'https://www.youtube.com/embed/8GZM3fhhC18?si=yyuxNxEsLkSoXg4B&amp', logo: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/DZRH_News_Television_Logo_%282021%29.jpg?20220522024804', category: "YouTube Live" },
+  { name: 'THE K-POP', type: "youtube", embedUrl: 'https://www.youtube.com/embed/JVocS7Yftw8?si=hSZxg1vZZB-xm9MN', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyIEniR-KegRyA_uAbvY7T0w7e0zMQTiX4ew&usqp=CAU', category: "YouTube Live" },
+  { name: 'THE K-POP 2', type: "youtube", embedUrl: 'https://www.youtube.com/embed/8fHegYsHwZQ?si=ZLSTB-VofWf2gbXR', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVY2Fz89o35Wx6ImHq8HVKrfp8dF-YXxrTGD6LcG7UZRclgSvkfnMXuYQ&s=10', category: "YouTube Live" }
 ];
