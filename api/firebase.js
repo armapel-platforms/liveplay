00efbda8f9d6746ab371dfb3a009ad10b06af607
Arbie Peligro <arbiemapalopeligro@gmail.com>
2025-09-04 17:13:53 +0800

    Create firebase.js

---
+++ api/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCU_G7QYIBVtb2kdEsQY6SF9skTuka-nfk",
+  authDomain: "liveplay-remote-project.firebaseapp.com",
+  databaseURL: "https://liveplay-remote-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "liveplay-remote-project",
+  storageBucket: "liveplay-remote-project.firebasestorage.app",
+  messagingSenderId: "135496487558",
+  appId: "1:135496487558:web:c2aad6f56157d245917707",
+  measurementId: "G-G9JXGMV4B8"
+};
