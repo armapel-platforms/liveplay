63e9447400d944a3c122a2d71f5266428fdb7c92
Arbie Peligro <arbiemapalopeligro@gmail.com>
2025-09-14 22:42:07 +0800

    Update and rename .htaccess to middleware.js

---
+++ middleware.js
@@ -0,0 +1,20 @@
+import { next } from '@vercel/edge';
+import { NextResponse } from 'next/server';
+
+const ALLOWED_COUNTRY = 'PH';
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|not-available|.*\\..*).*)',
+  ],
+};
+
+export default function middleware(request) {
+  const country = request.geo?.country;
+
+  if (country !== ALLOWED_COUNTRY) {
+    return NextResponse.rewrite(new URL('/not-available', request.url));
+  }
+
+  return next();
+}
