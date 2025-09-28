d9d49585c019bc20294aee34927f348fee1731a1
Arbie Peligro <arbiemapalopeligro@gmail.com>
2025-09-29 01:49:28 +0800

    Update yt-live.js

--- js/yt-live.js
+++ js/yt-live.js
@@ -1,10 +1,10 @@
 const yt_live = [
     { name: 'Kapamilya Online Live', logo: 'https://static.wikia.nocookie.net/logopedia/images/0/00/Kapamilya_Online_Live_3D_Logo_2020.png', type: 'youtube', embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCstEtN0pgOmCf02EdXsGChw', category: "YouTube Live" },
-    { name: 'Kapatid Livestream', logo: '/logo/kapatid_livestream_logo.png', type: 'youtube', embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCiFx0kZFjR29u0uJxTSq5MQ', category: "YouTube Live" },
+    { name: 'Kapatid Livestream', logo: '/assets/kapatid_livestream_logo.png', type: 'youtube', embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCiFx0kZFjR29u0uJxTSq5MQ', category: "YouTube Live" },
     { name: 'Kapuso Stream', logo: 'https://static.wikia.nocookie.net/gmanetwork/images/0/0f/Kapuso_Stream.png', type: "youtube", embedUrl: "https://www.youtube.com/embed/live_stream?channel=UCKL5hAuzgFQsyrsQKgU0Qng", category: "YouTube Live" },
     { name: 'DZRH TV', logo: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/DZRH_News_Television_Logo_%282021%29.jpg?20220522024804', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCcTiBX8js_djhSSlmJRI99A', category: "YouTube Live" },
-    { name: 'THE K-POP', logo: '/logo/the_kpop_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCoRXPcv8XK5fAplLbk9PTww', category: "YouTube Live" },
-    { name: 'THE K-POP 2', logo: '/logo/the_kpop2_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCITH7URIRpb8yoshUwGE9jg', category: "YouTube Live" },
-    { name: 'Vibing with K-POP' , logo: '/logo/vbngtkpop_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/jhsltv-xwQg', category: "YouTube Live" },
-    { name: 'MLBB PH Livestream' , logo: '/logo/mlbbph_livestream_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UC3QyDSID3tQBDRxMnfcfFiA', category: "YouTube Live" },
+    { name: 'THE K-POP', logo: '/assets/the_kpop_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCoRXPcv8XK5fAplLbk9PTww', category: "YouTube Live" },
+    { name: 'THE K-POP 2', logo: '/assets/the_kpop2_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UCITH7URIRpb8yoshUwGE9jg', category: "YouTube Live" },
+    { name: 'Vibing with K-POP' , logo: '/assets/vbngtkpop_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/jhsltv-xwQg', category: "YouTube Live" },
+    { name: 'MLBB PH Livestream' , logo: '/assets/mlbbph_livestream_logo.png', type: "youtube", embedUrl: 'https://www.youtube.com/embed/live_stream?channel=UC3QyDSID3tQBDRxMnfcfFiA', category: "YouTube Live" },
 ];
